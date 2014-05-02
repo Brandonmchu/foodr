92e012bf9d29fb0437460d1581f0f2c2a089cc17
Brandonmchu <brandon.mk.chu@gmail.com>
2014-05-01 20:38:39 -0400

    first commit, comes with Users, devise gem, bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodr::Application.config.secret_key_base = 'd3e853f3695804464fc9fc1f49233314d16ae8dda71b0eb0db73758db27dafebdcc1c2c77ec8741ad48634dc14bc9ee10977eccce3e6624f2ec7a99048d4259f'
