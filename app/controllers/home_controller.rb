6336e41e7316222e481d3df11702850eb00edad4
Brandonmchu <brandon.mk.chu@gmail.com>
2014-05-07 21:01:38 -0400

    refactors yelp response .map method so that it can handle a business without a specific property available without bombing the app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,11 +11,24 @@ end
 
 def yelpResult(args)
   yelpSearch = YelpSearch.searchYelp(args)
-	mappedResults = yelpSearch.businesses.map {|l| {id: l.id, name: l.name, categories:l.categories, rating: l.rating, review_count: l.review_count, url: l.url, phone: l.phone}}
+  mappedResults = map(yelpSearch)
   mappedResults.each do |result|
     Restaurant.create!(yelp_id: result[:id], name: result[:name]) unless Restaurant.find_by_yelp_id(result[:id])
   end
   return mappedResults
 end
 
+private
+  def map(yelpSearch)
+    yelpSearch.businesses.map {|l|
+    {
+      id: l.id,
+      name: l.respond_to?("name") ? l.name : "nameless",
+      categories: l.respond_to?("categories") ? l.categories : ["-"],
+      rating: l.respond_to?("rating") ? l.rating : "unrated",
+      review_count: l.respond_to?("review_count") ? l.review_count : "no reviews",
+      url: l.respond_to?("url") ? l.url : "no url",
+      phone: l.respond_to?("phone") ? l.phone : "no phone available"
+    }}
+  end
 end
